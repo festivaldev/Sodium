8454b789ead7f83a3114c0a035ce662531285fd0
vainamov <vainamov@users.noreply.github.com>
2017-11-29 18:38:15 +0100

    Initial commit

---
+++ Sodium/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Sodium
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
